a904fa77d0595929a6bb7e400741a30dcb649418
Thetiger97 <tuankiet.thptcamau@gmail.com>
2023-06-27 00:00:31 +0700

    Refactor code with correct format

--- src/main/java/com/tuankiet/employees/Main.java
+++ src/main/java/com/tuankiet/employees/Main.java
@@ -28,18 +28,6 @@ public class Main {
         Pattern peoplePat = Pattern.compile(peopleRegex);
         Matcher peopleMat = peoplePat.matcher(peopleText);
 
-        String progRegex = "\\w+=(?<locpd>\\w+),\\w+=(?<yoe>\\w+),\\w+=(?<iq>\\w+)";
-        Pattern coderPat = Pattern.compile(progRegex);
-
-        String mgrRegex = "\\w+=(?<orgSize>\\w+),\\w+=(?<dr>\\w+)";
-        Pattern mgrPat = Pattern.compile(mgrRegex);
-
-        String analystRegex = "\\w+=(?<projectCount>\\w+),\\w+=(?<dr>\\w+)";
-        Pattern analystPat = Pattern.compile(analystRegex);
-
-        String ceoRegex = "\\\\w+=(?<avgStockPrice>\\\\w+),\\\\w+=(?<dr>\\\\w+)\"";
-        Pattern ceoPat = Pattern.compile(ceoRegex);
-
         int totalSalaries = 0;
         while (peopleMat.find()) {
             totalSalaries+= switch (peopleMat.group("role")) {
